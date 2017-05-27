cb846e2cec772fb37746cc355699673a45ee9bf0
shankar <shankar@lamp-server.(none)>
2017-05-27 09:32:45 +0530

    added files

---
+++ file.sh
@@ -0,0 +1,6 @@
+rm -rf shankar
+mkdir shankar
+cd shankar
+rm -rf note.txt
+touch note.txt
+echo "hi how ru...hope u are good!!!this is my first script.Script excuted sucessfully!!!!!" > /home/shankar/shankar/note.txt   
